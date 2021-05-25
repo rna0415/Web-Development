f4fd6ab1eb3bfb86b7b3e74c0954605dda623127
Moses <67581910+rna0415@users.noreply.github.com>
2021-05-25 20:18:29 +0900

    Add files via upload

---
+++ components/Influencer/index.js
@@ -0,0 +1,4 @@
+export { default as UserWrapper } from './UserWrapper';
+export { default as UserContent } from './UserContent';
+export { default as Header } from './Header';
+export { default as InfluencerInfo } from './InfluencerInfo';
